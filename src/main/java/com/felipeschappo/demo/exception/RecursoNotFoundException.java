bb3d12a68b99ddc3d96298397e6afdd415acb38c
Felipe Schappo <felipe.schappo@crssystem.com.br>
2020-08-07 14:48:35 -0300

    Atividade Previa ari

---
+++ src/main/java/com/felipeschappo/demo/exception/RecursoNotFoundException.java
@@ -0,0 +1,9 @@
+package com.felipeschappo.demo.exception;
+
+public class RecursoNotFoundException extends RuntimeException {
+
+	public RecursoNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ src/main/java/com/felipeschappo/demo/repository/ClienteRepository.java
@@ -0,0 +1,14 @@
+package com.felipeschappo.demo.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.felipeschappo.demo.domain.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+	public Optional<Cliente> findByCpf(String cpf);
+	
+}
